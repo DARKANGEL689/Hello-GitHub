eb5d11b4d81625255d4fe2a8b1136fd43af71622
DARKANGEL689 <ahadsd1959083@gmail.com>
2026-01-22 21:32:15 +0530

    Day 1: Added C file for organizer project

---
+++ Day-01-File-Organizer/organizer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Day 1: File Organizer Project Started!\n");
+    printf("TODO: Add logic to scan directory...\n");
+    return 0;
+}
