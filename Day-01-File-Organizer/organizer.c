16a050f50129ae4b92428583acb9ddc8af8b74e9
DARKANGEL689 <ahadsd1959083@gmail.com>
2026-01-23 17:41:53 +0530

    Day 2: Added logic to move files and create folders

--- Day-01-File-Organizer/organizer.c
+++ Day-01-File-Organizer/organizer.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Day 1: File Organizer Project Started!\n");
-    printf("TODO: Add logic to scan directory...\n");
+    int choice;
+    
+    printf("--- FILE ORGANIZER (DAY 2) ---\n");
+    printf("This program will create 'Images' and 'Docs' folders.\n");
+    printf("It will move .jpg files to Images and .txt files to Docs.\n");
+    printf("Do you want to proceed? (1 for Yes, 0 for No): ");
+    
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+       
+        system("mkdir Images"); 
+        system("mkdir Docs"); 
+
+       
+        printf("\nMoving Images...\n");
+        system("move *.jpg Images");
+        
+        printf("\nMoving Text files...\n");
+        system("move *.txt Docs");
+        
+        printf("\nDone! Check your folder.\n");
+    } else {
+        printf("Operation cancelled.\n");
+    }
+
     return 0;
 }
